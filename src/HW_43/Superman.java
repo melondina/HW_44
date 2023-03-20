1dd94808b13733ff180545c5353029b8670361e1
Dina Migunova <migunova.dina@gmail.com>
2023-03-20 21:32:21 +0100

    Override in class Superman methods fly(),run(),swim()

--- src/HW_43/Superman.java
+++ src/HW_43/Superman.java
@@ -1,4 +1,18 @@
 package HW_43;
 
-public class Superman {
+public class Superman implements CanFly, CanRun, CanSwim {
+    @Override
+    public void fly() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    public void swim() {
+
+    }
 }
