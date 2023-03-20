9f95a64ce4d9dcb3b616cf0815c22389a7c01ff4
Dina Migunova <migunova.dina@gmail.com>
2023-03-20 21:57:53 +0100

    Rename class HW_44_Runner

--- src/HW_44/HW_44_Runner.java
+++ src/HW_44/HW_44_Runner.java
@@ -1,6 +1,6 @@
 package HW_44;
 
-public class HW_43_Runner {
+public class HW_44_Runner {
     public static void main(String[] args) {
         Duck testDuck = new Duck();
         testDuck.fly();
