21caca798056a57b2efd0930cd5c3ca3c83aabac
Dina Migunova <migunova.dina@gmail.com>
2023-03-20 21:45:40 +0100

    Change methods fly(),swim(),run() in class Superman

--- src/HW_43/Superman.java
+++ src/HW_43/Superman.java
@@ -3,16 +3,19 @@ package HW_43;
 public class Superman implements CanFly, CanRun, CanSwim {
     @Override
     public void fly() {
+        System.out.println("Superman can fly");
 
     }
 
     @Override
     public void run() {
+        System.out.println("Superman can run");
 
     }
 
     @Override
     public void swim() {
+        System.out.println("Superman can swim");
 
     }
 }
