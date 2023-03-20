663dd8b487d0c0e48ae3d08cf568df778776f446
Dina Migunova <migunova.dina@gmail.com>
2023-03-20 21:41:35 +0100

    Add class Penguin, override run(),swim(), fly().

---
+++ src/HW_43/Penguin.java
@@ -0,0 +1,19 @@
+package HW_43;
+
+public class Penguin implements CanSwim, CanRun, CanFly{
+    @Override
+    public void run() {
+        System.out.println("Penguins can run");
+    }
+
+    @Override
+    public void swim() {
+        System.out.println("Penguins can swim");
+
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("Penguins can`t fly");
+    }
+}
