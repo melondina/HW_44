b4024a5d589eecb3332d1bb07bc8356629e3d6d9
Dina Migunova <migunova.dina@gmail.com>
2023-03-20 21:43:08 +0100

    Change methods fly(),swim(),run() in class Duck

--- src/HW_43/Duck.java
+++ src/HW_43/Duck.java
@@ -3,16 +3,16 @@ package HW_43;
 public class Duck implements CanRun, CanSwim, CanFly{
     @Override
     public void fly() {
-
+        System.out.println("Ducks can fly");
     }
 
     @Override
     public void run() {
-
+        System.out.println("Ducks can run");
     }
 
     @Override
     public void swim() {
-
+        System.out.println("Ducks can swim");
     }
 }
