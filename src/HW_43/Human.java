64a5818d550178ad0343effaaa14bf14c4c47b3e
Dina Migunova <migunova.dina@gmail.com>
2023-03-20 21:44:35 +0100

    Change methods fly(),swim(),run() in class Human

--- src/HW_43/Human.java
+++ src/HW_43/Human.java
@@ -1,13 +1,21 @@
 package HW_43;
 
-public class Human implements CanSwim, CanRun{
+public class Human implements CanSwim, CanRun, CanFly{
     @Override
     public void run() {
+        System.out.println("Human can run");
 
     }
 
     @Override
     public void swim() {
+        System.out.println("Human can swim");
+
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("Human can`t fly");
 
     }
 }
