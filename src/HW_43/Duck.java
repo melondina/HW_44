96891fd0fbc1c28db6eb4fd3190a0ee323a1bf7d
Dina Migunova <migunova.dina@gmail.com>
2023-03-20 21:38:01 +0100

    Add class Duck, override run(),swim(), fly().

---
+++ src/HW_43/Duck.java
@@ -0,0 +1,18 @@
+package HW_43;
+
+public class Duck implements CanRun, CanSwim, CanFly{
+    @Override
+    public void fly() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    public void swim() {
+
+    }
+}
